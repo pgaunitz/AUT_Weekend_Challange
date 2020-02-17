7ab01635e25e03e7eb62ac9ef3d50ac8204edfd0
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-17 10:06:05 +0100

    new articels pass, fail on success msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+ 
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
